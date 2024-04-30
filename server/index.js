e7716630fc334113524d3fa9599db8a0f0114ffc
leeyoungjik1 <eog91@naver.com>
2024-04-30 14:16:52 +0900

    240430-2

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.send('hello world')
+})
+app.get('/error', (req, res) => {
+    throw new Error('서버 에러')
+})
+
+
+
+app.use((req, res, next) => {
+    res.status(404).send('페이지를 찾을수 없습니다.')
+})
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send('서버 에러 발생')
+})
+
+app.listen(5000, () => {
+    console.log('server is running on port 5000...')
+})
